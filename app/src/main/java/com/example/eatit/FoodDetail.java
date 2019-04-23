95a113734749b902954365cce3b71887468f920d
Hesham Shahtout <heshamshahtout@gmail.com>
2019-04-24 00:13:29 +0200

    Add food details

---
+++ app/src/main/java/com/example/eatit/FoodDetail.java
@@ -0,0 +1,91 @@
+package com.example.eatit;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.eatit.Model.Food;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class FoodDetail extends AppCompatActivity {
+
+    TextView food_name,food_price,food_description;
+    ImageView food_image;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    FloatingActionButton btnCart;
+    ElegantNumberButton numberButton;
+
+    String foodId="";
+
+    FirebaseDatabase database;
+    DatabaseReference foods;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        foods = database.getReference("Foods");
+
+        //Init view
+        numberButton = findViewById(R.id.number_button);
+        btnCart = findViewById(R.id.btnCart);
+
+        food_description = findViewById(R.id.food_description);
+        food_name = findViewById(R.id.food_name);
+        food_price = findViewById(R.id.food_price);
+        food_image = findViewById(R.id.food_image);
+
+        collapsingToolbarLayout = findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
+
+        //Get food id from intent
+        if(getIntent() != null)
+            foodId = getIntent().getStringExtra("FoodId");
+        if(!foodId.isEmpty())
+        {
+            getFoodDetails(foodId);
+        }
+
+    }
+
+    private void getFoodDetails(String foodId) {
+        foods.child(foodId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Food food = dataSnapshot.getValue(Food.class);
+
+                //Set Image
+                Picasso.with(getBaseContext()).load(food.getImage())
+                        .into(food_image);
+
+                collapsingToolbarLayout.setTitle(food.getName());
+
+                food_price.setText(food.getPrice());
+
+                food_name.setText(food.getName());
+
+                food_description.setText(food.getDescription());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/eatit/FoodList.java
+++ app/src/main/java/com/example/eatit/FoodList.java
@@ -1,5 +1,6 @@
 package com.example.eatit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -73,7 +74,10 @@ public class FoodList extends AppCompatActivity {
              viewHolder.setItemClickListner(new itemClickListner() {
                  @Override
                  public void onClick(View view, int position, boolean isLongClick) {
-                     Toast.makeText(FoodList.this, ""+local.getName(), Toast.LENGTH_SHORT).show();
+                     //Start new activity
+                     Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
+                     foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
+                     startActivity(foodDetail);
                  }
              });
          }
