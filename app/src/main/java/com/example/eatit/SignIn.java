7880f18b34c06e33eb48e2ea312f837bd662e79f
Hesham Shahtout <heshamshahtout@gmail.com>
2019-04-24 17:08:06 +0200

    Added Cart and User Order Submission

--- app/src/main/java/com/example/eatit/SignIn.java
+++ app/src/main/java/com/example/eatit/SignIn.java
@@ -52,8 +52,8 @@ public class SignIn extends AppCompatActivity {
                         if(dataSnapshot.child(edtPhone.getText().toString()).exists()){
                             mDialog.dismiss();
                         //Get user infromation
-
                         User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                        user.setPhone(edtPhone.getText().toString());
                         if(user.getPassword().equals(edtPassword.getText().toString()))
                         {
                             Intent homeIntent = new Intent(SignIn.this, Home.class);
